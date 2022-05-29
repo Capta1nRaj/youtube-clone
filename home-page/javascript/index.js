6f51b1f0379ffaa93a2a392eadd5c0e4a8e19cf7
Priyal Raj <101660221+Capta1nRaj@users.noreply.github.com>
2022-05-29 10:51:22 +0530

    Delete home-page directory

--- home-page/javascript/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    // Toggle Menu Left To Right Script
-    $('.menu-btn').click(function () {
-        $('.navigation-bar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    })
-});
