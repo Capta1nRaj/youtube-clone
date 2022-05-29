11b124a3ba2ee8876fda9baad4410a59042f227c
Priyal Raj <101660221+Capta1nRaj@users.noreply.github.com>
2022-05-29 05:45:51 +0530

    Add files via upload

---
+++ home-page/javascript/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    // Toggle Menu Left To Right Script
+    $('.menu-btn').click(function () {
+        $('.navigation-bar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    })
+});
