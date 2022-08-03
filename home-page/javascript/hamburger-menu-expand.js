a823809b38ce5622e4ac1b12604b26f1b208bfad
Capta1nRaj <priyalraj99@gmail.com>
2022-08-03 05:47:34 +0530

    No Message Hehe

--- home-page/javascript/hamburger-menu-expand.js
+++ home-page/javascript/hamburger-menu-expand.js
@@ -4,3 +4,12 @@ function hamburgerMenuExpand() {
         .classList
         .toggle("expand");
 }
+
+// Trial Mouse Hover
+var tooltipSpan = document.getElementsByClassName('youtube-home-tooltip-text')[0];
+window.onmousemove = function (e) {
+    var x = e.clientX,
+        y = e.clientY;
+    tooltipSpan.style.left = (x + 10) + 'px';
+    tooltipSpan.style.top = (y + 15) + 'px';
+};
